8297825899db8a63436a02133cf8cd406baa0790
Deepu.A <mrinfectanalyst@gmail.com>
2026-04-10 23:16:06 +0530

    Refactor apply method to enhance word transformations

--- src/engine/rules.cpp
+++ src/engine/rules.cpp
@@ -4,19 +4,19 @@
 std::vector<std::string> RuleEngine::apply(const std::string& word) {
     std::vector<std::string> results;
 
-    // Original
+ 
     results.push_back(word);
 
-    // Uppercase
+    
     std::string upper = word;
     std::transform(upper.begin(), upper.end(), upper.begin(), ::toupper);
     results.push_back(upper);
 
-    // Append numbers
+   
     results.push_back(word + "123");
     results.push_back(word + "2024");
 
-    // Leetspeak
+
     std::string leet = word;
     std::replace(leet.begin(), leet.end(), 'a', '4');
     std::replace(leet.begin(), leet.end(), 'e', '3');
